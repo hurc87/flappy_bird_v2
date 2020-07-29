db47e8a5af8c750bab652cb359da214ba322bfde
Zoe Hurcombe-straker <37640287+hurc87@users.noreply.github.com>
2020-07-29 20:10:25 +0100

    end of wed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # flappy_bird_v2
+
+tutoral - https://www.youtube.com/watch?v=gxHcW84izz0
+
+position absolute means it is positioned relative to its parent container. (based on parents dimensions and position)
+
+top - gives us spacing between top of parent element and our element
---
+++ app.js
@@ -0,0 +1,82 @@
+// Js will only run when all the elements in the html have loaded
+document.addEventListener('DOMContentLoaded', () => {
+
+    const bird = document.querySelector('.bird')
+    const gameDisplay = document.querySelector('.game-container')
+    const ground = document.querySelector('.ground')
+
+    let birdLeft = 220
+    let birdBottom = 100
+    let gravity = 2
+    let isGameOver = false
+
+    function startGame(){
+        // runs gravity each time the function is being invoked
+        birdBottom -= gravity
+        bird.style.bottom = birdBottom + 'px'
+        bird.style.left = birdLeft + 'px'
+    }
+
+    let gameTimerId = setInterval(startGame, 20)
+
+    function control(e){
+        if(e.keyCode === 32) {
+            jump()
+        }
+    }
+
+    function jump(){
+        // adding 50 to the birdBottom variable before its applied to the bird
+        if ( birdBottom < 500){
+            birdBottom += 50
+            bird.style.bottom = birdBottom + 'px'
+            console.log(birdBottom, 'bird bottom')
+        }
+    }
+
+    // key up triggers each time your finger leaves the key
+    document.addEventListener('keyup', control)
+
+    function generateObstacle(){
+        let obstacleLeft = 500
+        let randomHeight = Math.random() * 60
+        let obstacleBottom = randomHeight
+
+        const obstacle = document.createElement('div')
+        if(!isGameOver) obstacle.classList.add('obstacle')
+        gameDisplay.appendChild(obstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+
+        function moveObstacle(){
+            obstacleLeft -=2
+            obstacle.style.left = obstacleLeft + 'px'
+
+            if(obstacleLeft === -60){
+                clearInterval(timerId)
+                gameDisplay.removeChild(obstacle)
+            }
+
+            if(
+                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
+                birdBottom < obstacleBottom + 153 ||
+                birdBottom === 0){
+                gameOver()
+                clearInterval(timerId)
+            }
+        }
+        let timerId = setInterval(moveObstacle, 20)
+        if (!isGameOver) setTimeout(generateObstacle, 3000)
+    }
+
+    generateObstacle()
+
+    function gameOver(){
+        console.log('game over')
+        // clear the start game function running
+        clearInterval(gameTimerId)
+        isGameOver = true
+        document.removeEventListener('keyup', control)
+    }
+
+})
