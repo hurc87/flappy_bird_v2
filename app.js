c29954d87498c2967b7170757664e89bf03bfe8a
Zoe Hurcombe-straker <37640287+hurc87@users.noreply.github.com>
2020-07-30 19:24:08 +0100

    finished game

--- app.js
+++ app.js
@@ -41,25 +41,39 @@ document.addEventListener('DOMContentLoaded', () => {
         let obstacleLeft = 500
         let randomHeight = Math.random() * 60
         let obstacleBottom = randomHeight
+        let gap = 430
 
         const obstacle = document.createElement('div')
-        if(!isGameOver) obstacle.classList.add('obstacle')
+        const topObstacle = document.createElement('div')
+
+        if(!isGameOver) {
+            obstacle.classList.add('obstacle')
+            topObstacle.classList.add('topObstacle')
+        }
+    
         gameDisplay.appendChild(obstacle)
+        gameDisplay.appendChild(topObstacle)
+
         obstacle.style.left = obstacleLeft + 'px'
+        topObstacle.style.left = obstacleLeft + 'px'
+
         obstacle.style.bottom = obstacleBottom + 'px'
+        topObstacle.style.bottom = obstacleBottom + gap + 'px'
 
         function moveObstacle(){
             obstacleLeft -=2
             obstacle.style.left = obstacleLeft + 'px'
+            topObstacle.style.left = obstacleLeft + 'px'
 
             if(obstacleLeft === -60){
                 clearInterval(timerId)
                 gameDisplay.removeChild(obstacle)
+                gameDisplay.removeChild(topObstacle)
             }
 
             if(
                 obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
-                birdBottom < obstacleBottom + 153 ||
+                ( birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap - 200) ||
                 birdBottom === 0){
                 gameOver()
                 clearInterval(timerId)
